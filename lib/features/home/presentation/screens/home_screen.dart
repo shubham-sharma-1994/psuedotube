ff0201ec6b9f450a7ac54bdfb4f4825c486fe682
Shubham Sharma <106651538+shubham-sharma-1994@users.noreply.github.com>
2026-09-24 14:00:39 +0530

    fix(home): fix RecentPlaylistsSection const Sliver line

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -62,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverToBoxAdapter(
             child: SizedBox(height: AppDimens.spacingSm),
           ),
-          const (SliverToBoxAdapter(child: RecentPlaylistsSection())),
+          const SliverToBoxAdapter(child: RecentPlaylistsSection()),
           const SliverToBoxAdapter(child: LastPlayedSection()),
           const SliverToBoxAdapter(child: LikedSongsSection()),
           const SliverToBoxAdapter(child: FavoriteArtistsSection()),
